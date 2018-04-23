4d4e94a2542567eb2a00605511321fd13232761a
cbarron-siayec <38666243+cbarron-siayec@users.noreply.github.com>
2018-04-23 18:54:38 -0500

    Update selectAllSql.go

--- go/selectAllSql.go
+++ go/selectAllSql.go
@@ -4,5 +4,5 @@ import(
 _ "libs/sqlConnect"
 )
 func main(){
-  sqlConnect.SqlConnect("root","D3m0S14y3c","172.17.2.168","3306","municipios")
+  SqlConnect("root","D3m0S14y3c","172.17.2.168","3306","municipios")
 }
