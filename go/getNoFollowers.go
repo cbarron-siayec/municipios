3ad20c11d5e2a52a92bbc3af5911f2d2d0e4c420
cbarron-siayec <38666243+cbarron-siayec@users.noreply.github.com>
2018-04-24 10:29:13 -0500

    Create getNoFollowers.go

---
+++ go/getNoFollowers.go
@@ -0,0 +1,23 @@
+package getNoFollowers
+
+import(
+  "fmt"
+  "github.com/dghubble/go-twitter/twitter"
+  "github.com/dghubble/oauth1"
+)
+
+config := oauth1.NewConfig("U93DMt4ydkuj7KjtTQOH3A7DT", "dhpyVkyl6pxRDIvGsTcvCAQ9uNP2WJ9iFIAD2FlYCDY29nat1c
+")
+token := oauth1.NewToken("	2253585536-EAQIZqyv9Np8V1fJPcthYlT5TPvpacO0MK209j2", "OCgcI2poX3Y9fSVM0i2D0tWP9PepXmSiNJBRUMqAnmZU0
+")
+// http.Client will automatically authorize Requests
+httpClient := config.Client(oauth1.NoContext, token)
+
+// Twitter client
+client := twitter.NewClient(httpClient)
+
+followers, resp, err := client.Followers.List(&twitter.FollowerListParams{})
+
+func main(){
+  fmt.Printl("HOLA")
+}
