b384d6148b2197860dcf2ce766af030cf2a1eea6
cbarron-siayec <38666243+cbarron-siayec@users.noreply.github.com>
2018-04-26 13:42:40 -0500

    Update testFuncs.go

--- go/testFuncs.go
+++ go/testFuncs.go
@@ -7,6 +7,6 @@ import(
 )
 func main(){
  sqlConnect.SqlConnect("root","D3m0S14y3c","172.17.2.168","3306","municipios")
- numFollowers := twitterData.GetNumFollowers("tyI3dExsT4Q5LjiA5Oxs9r2rL","t6d19DqtSo7WjO8ChmX5dab8YOqw2kMv0yPHGW8EoAJYYGzL2i","119620082-ML9vIFx34BrmGNkKiK3scXwHjF7sisPfnwi1XqVs","9jpCnF2ixF8dSlEaJrXyRRHbfhxNOwAekZveDtbAAH2Uw","israel_kastillo")
+ numFollowers := twitterData.GetData("tyI3dExsT4Q5LjiA5Oxs9r2rL","t6d19DqtSo7WjO8ChmX5dab8YOqw2kMv0yPHGW8EoAJYYGzL2i","119620082-ML9vIFx34BrmGNkKiK3scXwHjF7sisPfnwi1XqVs","9jpCnF2ixF8dSlEaJrXyRRHbfhxNOwAekZveDtbAAH2Uw","israel_kastillo")
  fmt.Println(numFollowers)
 }
