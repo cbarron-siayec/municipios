76b3939d3a51e17075f78f0fcb13312507a26612
cbarron-siayec <38666243+cbarron-siayec@users.noreply.github.com>
2018-04-26 12:57:41 -0500

    Update testFuncs.go

--- go/testFuncs.go
+++ go/testFuncs.go
@@ -9,7 +9,7 @@ import(
 func main(){
  sqlConnect.SqlConnect("root","D3m0S14y3c","172.17.2.168","3306","municipios")
  numFollowers := getNumFollowers.HowManyFollowers("tyI3dExsT4Q5LjiA5Oxs9r2rL","t6d19DqtSo7WjO8ChmX5dab8YOqw2kMv0yPHGW8EoAJYYGzL2i","119620082-ML9vIFx34BrmGNkKiK3scXwHjF7sisPfnwi1XqVs","9jpCnF2ixF8dSlEaJrXyRRHbfhxNOwAekZveDtbAAH2Uw")
- numFollowersTest := getFollowersTest.GetNumFollowers("tyI3dExsT4Q5LjiA5Oxs9r2rL","t6d19DqtSo7WjO8ChmX5dab8YOqw2kMv0yPHGW8EoAJYYGzL2i","119620082-ML9vIFx34BrmGNkKiK3scXwHjF7sisPfnwi1XqVs","9jpCnF2ixF8dSlEaJrXyRRHbfhxNOwAekZveDtbAAH2Uw","realDonaldTrump")
+ numFollowersTest := getFollowersTest.GetNumFollowers("tyI3dExsT4Q5LjiA5Oxs9r2rL","t6d19DqtSo7WjO8ChmX5dab8YOqw2kMv0yPHGW8EoAJYYGzL2i","119620082-ML9vIFx34BrmGNkKiK3scXwHjF7sisPfnwi1XqVs","9jpCnF2ixF8dSlEaJrXyRRHbfhxNOwAekZveDtbAAH2Uw","Cristiano")
  fmt.Println(numFollowers)
  fmt.Println(numFollowersTest)
 }
