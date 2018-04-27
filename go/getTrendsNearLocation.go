ed23ffd0c691706175e8543e54febb78984611fa
cbarron-siayec <38666243+cbarron-siayec@users.noreply.github.com>
2018-04-27 14:14:42 -0500

    Update getTrendsNearLocation.go

--- go/getTrendsNearLocation.go
+++ go/getTrendsNearLocation.go
@@ -2,14 +2,14 @@ package main
 import (
   "fmt"
   "github.com/ChimeraCoder/anaconda"
+  "net/url"
 )
 
 func main(){
   anaconda.SetConsumerKey("tyI3dExsT4Q5LjiA5Oxs9r2rL")
   anaconda.SetConsumerSecret("t6d19DqtSo7WjO8ChmX5dab8YOqw2kMv0yPHGW8EoAJYYGzL2i")
   api := anaconda.NewTwitterApi("119620082-ML9vIFx34BrmGNkKiK3scXwHjF7sisPfnwi1XqVs","9jpCnF2ixF8dSlEaJrXyRRHbfhxNOwAekZveDtbAAH2Uw")
-  u := url.Parse("https://api.twitter.com/1.1/trends/place")
-  v := u.Values{}
+  v := url.Values{}
   v.Set("exclude", "hastags")
   trends, err := api.GetTrendsByPlace(24550705,v)
   if err != nil {
