3b917dcd3e9569550f76dab575c7c5c15ced791f
cbarron-siayec <38666243+cbarron-siayec@users.noreply.github.com>
2018-04-27 17:44:46 -0500

    Update getTrendsByLocation.go

--- go/getTrendsByLocation.go
+++ go/getTrendsByLocation.go
@@ -14,7 +14,7 @@ func main(){
   	// Twitter client
   	client := twitter.NewClient(httpClient)
 	
-	trends,resp,err := client.Trends.Place(24550705,&twitter.TrendsPlaceParams{WOEID:24550705})
+	trends,resp,err := client.SHIAAAT.Place(24550705,&twitter.TrendsPlaceParams{WOEID:24550705})
     	if err != nil {
       		panic(err.Error())
     	}
