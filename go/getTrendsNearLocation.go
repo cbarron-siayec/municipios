a878827ffd359b5859a5046202f333aae07f4f97
cbarron-siayec <38666243+cbarron-siayec@users.noreply.github.com>
2018-04-27 12:58:29 -0500

    Update getTrendsNearLocation.go

--- go/getTrendsNearLocation.go
+++ go/getTrendsNearLocation.go
@@ -5,9 +5,8 @@ import (
 )
 
 func main(){
-  anaconda.SetConsumerKey("D3hzZ2y3AlvG8o8Xw8SE2fxvZ")
-  anaconda.SetConsumerSecret("gpAkuWx7cyfGY6m6XffqcDyxNdwZRxhoCGDGS7ra54tCp1BHY5")
-  api := anaconda.NewTwitterApi("2253585536-Yy1AxlyCNmEfEyJIOmhByqejD7XMXTpBIIHXoFW","Ha8IhwH4YElQwP2K3k3yKeT4EyDvZfd4EuxadGcddII9e")
+  api := anaconda.NewTwitterApiWithCredentials("tyI3dExsT4Q5LjiA5Oxs9r2rL","t6d19DqtSo7WjO8ChmX5dab8YOqw2kMv0yPHGW8EoAJYYGzL2i","119620082-ML9vIFx34BrmGNkKiK3scXwHjF7sisPfnwi1XqVs","9jpCnF2ixF8dSlEaJrXyRRHbfhxNOwAekZveDtbAAH2Uw")
+  fmt.Println(*api.Credentials)
   trends, err := api.GetTrendsByPlace(24550705,nil)
   if err != nil {
 	  panic(err.Error())
