b2db09d1237327da55d627848cd67ff165a875cc
cbarron-siayec <38666243+cbarron-siayec@users.noreply.github.com>
2018-04-27 11:55:46 -0500

    Update getTrendsNearLocation.go

--- go/getTrendsNearLocation.go
+++ go/getTrendsNearLocation.go
@@ -6,8 +6,10 @@ import (
 
 func main(){
   api := anaconda.NewTwitterApiWithCredentials("D3hzZ2y3AlvG8o8Xw8SE2fxvZ", "gpAkuWx7cyfGY6m6XffqcDyxNdwZRxhoCGDGS7ra54tCp1BHY5", "2253585536-Yy1AxlyCNmEfEyJIOmhByqejD7XMXTpBIIHXoFW", "Ha8IhwH4YElQwP2K3k3yKeT4EyDvZfd4EuxadGcddII9e")
-  searchResult, _ := api.GetSearch("realDonaldTrump", nil)
-  for _ , tweet := range searchResult.Statuses {
-      fmt.Println(tweet.Text)
+  api, trends, resp := GetTrendsByPlace(24550705,"")
+  if resp != nil {
+    fmt.Println("Error")
   }
+  fmt.Println(trends)
+  fmt.Println(resp)
 }
