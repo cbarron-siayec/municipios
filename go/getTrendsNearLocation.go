1eaff96c59792450a7b72d61f008adbf6c318a9e
cbarron-siayec <38666243+cbarron-siayec@users.noreply.github.com>
2018-04-27 12:14:12 -0500

    Update getTrendsNearLocation.go

--- go/getTrendsNearLocation.go
+++ go/getTrendsNearLocation.go
@@ -6,7 +6,7 @@ import (
 
 func main(){
   api := anaconda.NewTwitterApiWithCredentials("D3hzZ2y3AlvG8o8Xw8SE2fxvZ", "gpAkuWx7cyfGY6m6XffqcDyxNdwZRxhoCGDGS7ra54tCp1BHY5", "2253585536-Yy1AxlyCNmEfEyJIOmhByqejD7XMXTpBIIHXoFW", "Ha8IhwH4YElQwP2K3k3yKeT4EyDvZfd4EuxadGcddII9e")
-  trends := api.GetTrendsByPlace(24550705,"")
+  trends := api.GetTrendsByPlace(24550705)
   for page := range trends {
 		//Print the current page of followers
 		fmt.Println(page.Followers)
