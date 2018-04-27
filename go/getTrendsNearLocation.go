230d26ea708781928bcd9900ee89f03f999b5aa8
cbarron-siayec <38666243+cbarron-siayec@users.noreply.github.com>
2018-04-27 17:39:24 -0500

    Update getTrendsNearLocation.go

--- go/getTrendsNearLocation.go
+++ go/getTrendsNearLocation.go
@@ -7,8 +7,8 @@ import (
 )
 
 func main(){
-	config := oauth1.NewConfig(consumerKey,consumerSecret)
-	token := oauth1.NewToken(accessToken,tokenSecret)
+	config := oauth1.NewConfig("D3hzZ2y3AlvG8o8Xw8SE2fxvZ","gpAkuWx7cyfGY6m6XffqcDyxNdwZRxhoCGDGS7ra54tCp1BHY5")
+	token := oauth1.NewToken("2253585536-Yy1AxlyCNmEfEyJIOmhByqejD7XMXTpBIIHXoFW","Ha8IhwH4YElQwP2K3k3yKeT4EyDvZfd4EuxadGcddII9e")
   	// http.Client will automatically authorize Requests
   	httpClient := config.Client(oauth1.NoContext, token)
   	// Twitter client
