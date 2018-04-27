0ba0ef31223bb5cc781d136ac3f0bf7a470ee362
cbarron-siayec <38666243+cbarron-siayec@users.noreply.github.com>
2018-04-27 11:42:35 -0500

    Update getTrendsNearLocation.go

--- go/getTrendsNearLocation.go
+++ go/getTrendsNearLocation.go
@@ -1,26 +1,13 @@
 package main
 import (
   "fmt"
-  "github.com/dghubble/go-twitter/twitter"
-  "github.com/dghubble/oauth1"
+  "github.com/ChimeraCoder/anaconda"
 )
 
 func main(){
-  
-  config := oauth1.NewConfig("D3hzZ2y3AlvG8o8Xw8SE2fxvZ","gpAkuWx7cyfGY6m6XffqcDyxNdwZRxhoCGDGS7ra54tCp1BHY5")
-  token := oauth1.NewToken("2253585536-Yy1AxlyCNmEfEyJIOmhByqejD7XMXTpBIIHXoFW","Ha8IhwH4YElQwP2K3k3yKeT4EyDvZfd4EuxadGcddII9e")
-  // http.Client will automatically authorize Requests
-  httpClient := config.Client(oauth1.NoContext, token)
-  // Twitter client
-  client := twitter.NewClient(httpClient)
-  
-  trends,resp,err := client.Trends.Place(24550705,&twitter.TrendsPlaceParams{WOEID:24550705})
-    if err != nil {
-      panic(err.Error())
-    }
-    
-    fmt.Println(resp)
-    fmt.Println("------")
-    fmt.Println("TRENDS")
-    fmt.Println(trends[0])
+  api := anaconda.NewTwitterApiWithCredentials("D3hzZ2y3AlvG8o8Xw8SE2fxvZ", "gpAkuWx7cyfGY6m6XffqcDyxNdwZRxhoCGDGS7ra54tCp1BHY5", "2253585536-Yy1AxlyCNmEfEyJIOmhByqejD7XMXTpBIIHXoFW", "Ha8IhwH4YElQwP2K3k3yKeT4EyDvZfd4EuxadGcddII9e")
+  searchResult, _ := api.GetSearch("golang", nil)
+  for _ , tweet := range searchResult.Statuses {
+      fmt.Println(tweet.Text)
+  }
 }
