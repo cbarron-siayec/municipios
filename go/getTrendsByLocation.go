c3c0a4ca2fb8e5639fcbe1fe5c402675288dc295
Carlos Barrón MAC <cbarron@grupo-siayec.com.mx>
2018-04-29 10:56:27 -0500

    Funcion para obtener trends por WOEID

--- go/getTrendsByLocation.go
+++ go/getTrendsByLocation.go
@@ -1,27 +1,36 @@
-package main
+package getTrendsByLocation
 
 import (
 	"fmt"
+	"strconv"
+
 	"github.com/dghubble/go-twitter/twitter"
- 	"github.com/dghubble/oauth1"
+	"github.com/dghubble/oauth1"
 )
 
-func main(){
-	config := oauth1.NewConfig("D3hzZ2y3AlvG8o8Xw8SE2fxvZ","gpAkuWx7cyfGY6m6XffqcDyxNdwZRxhoCGDGS7ra54tCp1BHY5")
-	token := oauth1.NewToken("2253585536-Yy1AxlyCNmEfEyJIOmhByqejD7XMXTpBIIHXoFW","Ha8IhwH4YElQwP2K3k3yKeT4EyDvZfd4EuxadGcddII9e")
-  	// http.Client will automatically authorize Requests
-  	httpClient := config.Client(oauth1.NoContext, token)
-  	// Twitter client
-  	client := twitter.NewClient(httpClient)
-	trends,resp,err := client.Trends.Place(116545,&twitter.TrendsPlaceParams{WOEID:116545})
-	
-    	if err != nil {
-      		panic(err.Error())
-    	}
-	if resp != nil {
-		fmt.Println(resp)
-    	}
-	for i:=0;i<=len(trends.Trends);i++ {
-		fmt.Println(trends.Trends)
+func GetTrends(WOEID int64) [50]string {
+	config := oauth1.NewConfig("D3hzZ2y3AlvG8o8Xw8SE2fxvZ", "gpAkuWx7cyfGY6m6XffqcDyxNdwZRxhoCGDGS7ra54tCp1BHY5")
+	token := oauth1.NewToken("2253585536-Yy1AxlyCNmEfEyJIOmhByqejD7XMXTpBIIHXoFW", "Ha8IhwH4YElQwP2K3k3yKeT4EyDvZfd4EuxadGcddII9e")
+	// http.Client will automatically authorize Requests
+	httpClient := config.Client(oauth1.NoContext, token)
+	// Twitter client
+	client := twitter.NewClient(httpClient)
+	trends, resp, err := client.Trends.Place(WOEID, &twitter.TrendsPlaceParams{WOEID: WOEID})
+
+	if err != nil {
+		panic(err.Error())
 	}
+	if resp == nil {
+		fmt.Println("No Response!")
+	}
+	var top50 [50]string
+	for i := 0; i < len(trends); i++ {
+		for j := 0; j < len(trends[i].Trends); j++ {
+			names := trends[i].Trends[j].Name
+			volumes := strconv.FormatInt(trends[i].Trends[j].TweetVolume, 10)
+			concRes := "Name: " + names + ", Volume: " + volumes
+			top50[j] = concRes
+		}
+	}
+	return top50
 }
--- go/testFuncs.go
+++ go/testFuncs.go
@@ -1,12 +1,44 @@
 package main
 
-import(
- "libs/sqlConnect"
- "libs/twitterData"
- "fmt"
+import (
+	"fmt"
+	"strconv"
+
+	"github.com/dghubble/go-twitter/twitter"
+	"github.com/dghubble/oauth1"
 )
-func main(){
- sqlConnect.SqlConnect("root","D3m0S14y3c","172.17.2.168","3306","municipios")
- numFollowers := twitterData.GetData("tyI3dExsT4Q5LjiA5Oxs9r2rL","t6d19DqtSo7WjO8ChmX5dab8YOqw2kMv0yPHGW8EoAJYYGzL2i","119620082-ML9vIFx34BrmGNkKiK3scXwHjF7sisPfnwi1XqVs","9jpCnF2ixF8dSlEaJrXyRRHbfhxNOwAekZveDtbAAH2Uw","little_sul")
- fmt.Println(numFollowers)
+
+func GetTrends(WOEID int64) [50]string {
+	config := oauth1.NewConfig("D3hzZ2y3AlvG8o8Xw8SE2fxvZ", "gpAkuWx7cyfGY6m6XffqcDyxNdwZRxhoCGDGS7ra54tCp1BHY5")
+	token := oauth1.NewToken("2253585536-Yy1AxlyCNmEfEyJIOmhByqejD7XMXTpBIIHXoFW", "Ha8IhwH4YElQwP2K3k3yKeT4EyDvZfd4EuxadGcddII9e")
+	// http.Client will automatically authorize Requests
+	httpClient := config.Client(oauth1.NoContext, token)
+	// Twitter client
+	client := twitter.NewClient(httpClient)
+	trends, resp, err := client.Trends.Place(WOEID, &twitter.TrendsPlaceParams{WOEID: WOEID})
+
+	if err != nil {
+		panic(err.Error())
+	}
+	if resp == nil {
+		fmt.Println("No Response!")
+	}
+	var top50 [50]string
+	for i := 0; i < len(trends); i++ {
+		for j := 0; j < len(trends[i].Trends); j++ {
+			names := trends[i].Trends[j].Name
+			volumes := strconv.FormatInt(trends[i].Trends[j].TweetVolume, 10)
+			concRes := "Name: " + names + ", Volume: " + volumes
+			top50[j] = concRes
+		}
+	}
+	return top50
+}
+func main() {
+	resArray := GetTrends(151582)
+	for k := 0; k < len(resArray); k++ {
+		if resArray[k] != "" {
+			fmt.Println(resArray[k])
+		}
+	}
 }
