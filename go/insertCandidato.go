17980b27d2fea6017db8fd25cbae2adc314e43f6
cbarron-siayec <38666243+cbarron-siayec@users.noreply.github.com>
2018-04-30 13:12:27 -0500

    Update insertCandidato.go

--- go/insertCandidato.go
+++ go/insertCandidato.go
@@ -3,12 +3,12 @@ package insertCandidato
 import (
   "fmt"
   _ "github.com/go-sql-driver/mysql"
-  "database/sql"
+  _ "database/sql"
   "libs/sqlConnect"
 )
 
 func InsertCandidato(){
-        sqlConnect.SqlConnect(root, D3m0S14y3c, "172.17.2.168", 3306, candidatos)
+        sqlConnect.SqlConnect("root","D3m0S14y3c", "172.17.2.168",3306,"candidatos")
         stmt, err := db.Prepare("INSERT twitterData SET idCandidatos=?,friends=?,favorites=?,followers=?,lists=?,favorites=?,tweets=?")
         checkErr(err)
 
