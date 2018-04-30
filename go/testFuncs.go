f8561476e2c8a2b7767c584d28d4c8b729fff552
cbarron-siayec <38666243+cbarron-siayec@users.noreply.github.com>
2018-04-30 12:42:50 -0500

    Update testFuncs.go

--- go/testFuncs.go
+++ go/testFuncs.go
@@ -2,43 +2,10 @@ package main
 
 import (
 	"fmt"
-	"strconv"
-
-	"github.com/dghubble/go-twitter/twitter"
-	"github.com/dghubble/oauth1"
+	"libs/twitterData"
 )
 
-func GetTrends(WOEID int64) [50]string {
-	config := oauth1.NewConfig("D3hzZ2y3AlvG8o8Xw8SE2fxvZ", "gpAkuWx7cyfGY6m6XffqcDyxNdwZRxhoCGDGS7ra54tCp1BHY5")
-	token := oauth1.NewToken("2253585536-Yy1AxlyCNmEfEyJIOmhByqejD7XMXTpBIIHXoFW", "Ha8IhwH4YElQwP2K3k3yKeT4EyDvZfd4EuxadGcddII9e")
-	// http.Client will automatically authorize Requests
-	httpClient := config.Client(oauth1.NoContext, token)
-	// Twitter client
-	client := twitter.NewClient(httpClient)
-	trends, resp, err := client.Trends.Place(WOEID, &twitter.TrendsPlaceParams{WOEID: WOEID})
-
-	if err != nil {
-		panic(err.Error())
-	}
-	if resp == nil {
-		fmt.Println("No Response!")
-	}
-	var top50 [50]string
-	for i := 0; i < len(trends); i++ {
-		for j := 0; j < len(trends[i].Trends); j++ {
-			names := trends[i].Trends[j].Name
-			volumes := strconv.FormatInt(trends[i].Trends[j].TweetVolume, 10)
-			concRes := "Name: " + names + ", Volume: " + volumes
-			top50[j] = concRes
-		}
-	}
-	return top50
-}
 func main() {
-	resArray := GetTrends(151582)
-	for k := 0; k < len(resArray); k++ {
-		if resArray[k] != "" {
-			fmt.Println(resArray[k])
-		}
-	}
+	test := twitterData.GetData("D3hzZ2y3AlvG8o8Xw8SE2fxvZ","gpAkuWx7cyfGY6m6XffqcDyxNdwZRxhoCGDGS7ra54tCp1BHY5","2253585536-Yy1AxlyCNmEfEyJIOmhByqejD7XMXTpBIIHXoFW","Ha8IhwH4YElQwP2K3k3yKeT4EyDvZfd4EuxadGcddII9e","lopezobrador_", 1)
+	fmt.Println(test)
 }
