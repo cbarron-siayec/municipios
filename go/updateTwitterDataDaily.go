d5f7edffce1b34a94bd4cda3aeea218854b2ebba
cbarron-siayec <38666243+cbarron-siayec@users.noreply.github.com>
2018-04-30 16:16:05 -0500

    Create updateTwitterDataDaily.go

---
+++ go/updateTwitterDataDaily.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"libs/twitterData"
+	"libs/insertCandidato"
+)
+
+func main() {
+	amlo := twitterData.GetData("D3hzZ2y3AlvG8o8Xw8SE2fxvZ","gpAkuWx7cyfGY6m6XffqcDyxNdwZRxhoCGDGS7ra54tCp1BHY5","2253585536-Yy1AxlyCNmEfEyJIOmhByqejD7XMXTpBIIHXoFW","Ha8IhwH4YElQwP2K3k3yKeT4EyDvZfd4EuxadGcddII9e","lopezobrador_", 1)
+	rac := twitterData.GetData("D3hzZ2y3AlvG8o8Xw8SE2fxvZ","gpAkuWx7cyfGY6m6XffqcDyxNdwZRxhoCGDGS7ra54tCp1BHY5","2253585536-Yy1AxlyCNmEfEyJIOmhByqejD7XMXTpBIIHXoFW","Ha8IhwH4YElQwP2K3k3yKeT4EyDvZfd4EuxadGcddII9e","RicardoAnayaC", 2)
+	jamk := twitterData.GetData("D3hzZ2y3AlvG8o8Xw8SE2fxvZ","gpAkuWx7cyfGY6m6XffqcDyxNdwZRxhoCGDGS7ra54tCp1BHY5","2253585536-Yy1AxlyCNmEfEyJIOmhByqejD7XMXTpBIIHXoFW","Ha8IhwH4YElQwP2K3k3yKeT4EyDvZfd4EuxadGcddII9e","JoseAMeadeK", 3)
+	mz := twitterData.GetData("D3hzZ2y3AlvG8o8Xw8SE2fxvZ","gpAkuWx7cyfGY6m6XffqcDyxNdwZRxhoCGDGS7ra54tCp1BHY5","2253585536-Yy1AxlyCNmEfEyJIOmhByqejD7XMXTpBIIHXoFW","Ha8IhwH4YElQwP2K3k3yKeT4EyDvZfd4EuxadGcddII9e","Mzavalagc", 4)
+	bronco := twitterData.GetData("D3hzZ2y3AlvG8o8Xw8SE2fxvZ","gpAkuWx7cyfGY6m6XffqcDyxNdwZRxhoCGDGS7ra54tCp1BHY5","2253585536-Yy1AxlyCNmEfEyJIOmhByqejD7XMXTpBIIHXoFW","Ha8IhwH4YElQwP2K3k3yKeT4EyDvZfd4EuxadGcddII9e","JaimeRdzNL", 5)
+	// ORDEEN INSERT CANDIDATO: idCandidato int,friends int, favorites int, followers int, lists int, tweets int
+	// NOMBRES ESTRUCTURA USER: IDCandidato,FriendsCount,FavouritesCount,FollowersCount,ListCount,NoTweets
+	//PARA AMLO
+	insertCandidato.InsertCandidato(amlo.IDCandidato,amlo.FriendsCount,amlo.FavouritesCount,amlo.FollowersCount,amlo.ListCount,amlo.NoTweets)
+	//PARA RAC
+	insertCandidato.InsertCandidato(rac.IDCandidato,rac.FriendsCount,rac.FavouritesCount,rac.FollowersCount,rac.ListCount,rac.NoTweets)
+	//PARA JAMK
+	insertCandidato.InsertCandidato(jamk.IDCandidato,jamk.FriendsCount,jamk.FavouritesCount,jamk.FollowersCount,jamk.ListCount,jamk.NoTweets)
+	//PARA MZ
+	insertCandidato.InsertCandidato(mz.IDCandidato,mz.FriendsCount,mz.FavouritesCount,mz.FollowersCount,mz.ListCount,mz.NoTweets)
+	//PARA BRONCO
+	insertCandidato.InsertCandidato(bronco.IDCandidato,bronco.FriendsCount,bronco.FavouritesCount,bronco.FollowersCount,bronco.ListCount,bronco.NoTweets)
+}
