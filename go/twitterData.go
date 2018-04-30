de57cef9dca82b647fdcb56c15d3be0ba4ac21d1
cbarron-siayec <38666243+cbarron-siayec@users.noreply.github.com>
2018-04-30 12:24:57 -0500

    Update twitterData.go

--- go/twitterData.go
+++ go/twitterData.go
@@ -7,53 +7,17 @@ import(
 )
 
 type User struct {
-	ContributorsEnabled            bool          `json:"contributors_enabled"`
-	CreatedAt                      string        `json:"created_at"`
-	DefaultProfile                 bool          `json:"default_profile"`
-	DefaultProfileImage            bool          `json:"default_profile_image"`
-	Description                    string        `json:"description"`
-	Email                          string        `json:"email"`
-	Entities                       *UserEntities `json:"entities"`
 	FavouritesCount                int           `json:"favourites_count"`
-	FollowRequestSent              bool          `json:"follow_request_sent"`
-	Following                      bool          `json:"following"`
 	FollowersCount                 int           `json:"followers_count"`
 	FriendsCount                   int           `json:"friends_count"`
-	GeoEnabled                     bool          `json:"geo_enabled"`
 	ID                             int64         `json:"id"`
-	IDStr                          string        `json:"id_str"`
-	IsTranslator                   bool          `json:"is_translator"`
-	Lang                           string        `json:"lang"`
-	ListedCount                    int           `json:"listed_count"`
-	Location                       string        `json:"location"`
-	Name                           string        `json:"name"`
-	Notifications                  bool          `json:"notifications"`
-	ProfileBackgroundColor         string        `json:"profile_background_color"`
-	ProfileBackgroundImageURL      string        `json:"profile_background_image_url"`
-	ProfileBackgroundImageURLHttps string        `json:"profile_background_image_url_https"`
-	ProfileBackgroundTile          bool          `json:"profile_background_tile"`
-	ProfileBannerURL               string        `json:"profile_banner_url"`
-	ProfileImageURL                string        `json:"profile_image_url"`
-	ProfileImageURLHttps           string        `json:"profile_image_url_https"`
-	ProfileLinkColor               string        `json:"profile_link_color"`
-	ProfileSidebarBorderColor      string        `json:"profile_sidebar_border_color"`
-	ProfileSidebarFillColor        string        `json:"profile_sidebar_fill_color"`
-	ProfileTextColor               string        `json:"profile_text_color"`
-	ProfileUseBackgroundImage      bool          `json:"profile_use_background_image"`
-	Protected                      bool          `json:"protected"`
 	ScreenName                     string        `json:"screen_name"`
-	ShowAllInlineMedia             bool          `json:"show_all_inline_media"`
-	Status                         *Tweet        `json:"status"`
 	StatusesCount                  int           `json:"statuses_count"`
-	Timezone                       string        `json:"time_zone"`
-	URL                            string        `json:"url"`
-	UtcOffset                      int           `json:"utc_offset"`
-	Verified                       bool          `json:"verified"`
-	WithheldInCountries            []string      `json:"withheld_in_countries"`
-	WithholdScope                  string        `json:"withheld_scope"`
+	ListCount		       int	     'json:list_count'
+	IDCandidato		       int	     'json:IDCandidato'
 }
 
-func GetData(consumerKey string, consumerSecret string,accessToken string, tokenSecret string,screenName string) User{
+func GetData(consumerKey string, consumerSecret string,accessToken string, tokenSecret string,screenName string, idCandidato int) User{
   config := oauth1.NewConfig(consumerKey,consumerSecret)
   token := oauth1.NewToken(accessToken,tokenSecret)
   // http.Client will automatically authorize Requests
@@ -66,19 +30,10 @@ func GetData(consumerKey string, consumerSecret string,accessToken string, token
       panic(err.Error())
     }
   
-  //https://developer.twitter.com/en/docs/accounts-and-users/follow-search-get-users/api-reference/get-followers-list
-  //noFollowers := show.FollowersCount
-  //noFriends := show.FriendsCount
-  //createdDate := show.CreatedAt
-  //email := show.Email
-  //noFavs := show.FavouritesCount
-  //noFollowing := show.Following
-  //geoEnabled := show.GeoEnabled
-  //userId := show.ID
-  //noListed := show.ListedCount
-  //noTweets := show.StatusesCount
+  //https://developer.twitter.com/en/docs/accounts-and-users/follow-search-get-users/api-reference/get-followers-list 
+  twitterInfo := User{FavouritesCount: show.FavouritesCount, FollowersCount: show.FollowersCount,FriendsCount:show.FriendsCount,ScreenName:show.ScreenName,ID:show.ID,ListCount:show.ListedCount,noTweets:show.StatusesCount,IDCandidato:idCandidato}
   if resp == nil {
     fmt.Println("No Response")
   }
-  return show  
+  return twitterInfo
 }
