db30518d85577e345e662c245b5229c7398880f4
cbarron-siayec <38666243+cbarron-siayec@users.noreply.github.com>
2018-04-30 12:01:41 -0500

    Update twitterData.go

--- go/twitterData.go
+++ go/twitterData.go
@@ -6,7 +6,7 @@ import(
   "github.com/dghubble/oauth1"
 )
 
-func GetData(consumerKey string, consumerSecret string,accessToken string, tokenSecret string,screenName string) int{
+func GetData(consumerKey string, consumerSecret string,accessToken string, tokenSecret string,screenName string) User{
   config := oauth1.NewConfig(consumerKey,consumerSecret)
   token := oauth1.NewToken(accessToken,tokenSecret)
   // http.Client will automatically authorize Requests
@@ -20,37 +20,18 @@ func GetData(consumerKey string, consumerSecret string,accessToken string, token
     }
   
   //https://developer.twitter.com/en/docs/accounts-and-users/follow-search-get-users/api-reference/get-followers-list
-  noFollowers := show.FollowersCount
-  noFriends := show.FriendsCount
-  createdDate := show.CreatedAt
-  email := show.Email
-  noFavs := show.FavouritesCount
-  noFollowing := show.Following
-  geoEnabled := show.GeoEnabled
-  userId := show.ID
-  noListed := show.ListedCount
-  noTweets := show.StatusesCount
+  //noFollowers := show.FollowersCount
+  //noFriends := show.FriendsCount
+  //createdDate := show.CreatedAt
+  //email := show.Email
+  //noFavs := show.FavouritesCount
+  //noFollowing := show.Following
+  //geoEnabled := show.GeoEnabled
+  //userId := show.ID
+  //noListed := show.ListedCount
+  //noTweets := show.StatusesCount
   if resp == nil {
     fmt.Println("No Response")
   }
-  fmt.Println("No Friends: ")
-  fmt.Println(noFriends)
-  fmt.Println("Created: ")
-  fmt.Println(createdDate)
-  fmt.Println("EMAIL: ")
-  fmt.Println(email)
-  fmt.Println("No Favs: ")
-  fmt.Println(noFavs)
-  fmt.Println("Following: ")
-  fmt.Println(noFollowing)
-  fmt.Println("Geo Enabled: ")
-  fmt.Println(geoEnabled)
-  fmt.Println("User ID")
-  fmt.Println(userId)
-  fmt.Println("No Listed: ")
-  fmt.Println(noListed)
-  fmt.Println("No Tweets: ")
-  fmt.Println(noTweets)
-  fmt.Println("No Followers: ")
-  return noFollowers
+  return show  
 }
