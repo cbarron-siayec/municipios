387e176ab6fc7cbf498e2f491f3aa9e22c925c67
cbarron-siayec <38666243+cbarron-siayec@users.noreply.github.com>
2018-04-30 13:11:32 -0500

    Update insertCandidato.go

--- go/insertCandidato.go
+++ go/insertCandidato.go
@@ -8,8 +8,8 @@ import (
 )
 
 func InsertCandidato(){
-        sqlConnect.SqlConnect(root, D3m0S14y3c, 172.17.2.168, 3306, candidatos)
-        stmt, err := db.Prepare("INSERT twitterData SET idCandidatos=?,friends=?,favorites=?",followers=?",lists=?",favorites=?",tweets=?")
+        sqlConnect.SqlConnect(root, D3m0S14y3c, "172.17.2.168", 3306, candidatos)
+        stmt, err := db.Prepare("INSERT twitterData SET idCandidatos=?,friends=?,favorites=?,followers=?,lists=?,favorites=?,tweets=?")
         checkErr(err)
 
         res, err := stmt.Exec(1,1,1,1,1,1,1)
