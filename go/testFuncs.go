dbde3d93dad1d3cc95b95abc1403e86530eca2b1
cbarron-siayec <38666243+cbarron-siayec@users.noreply.github.com>
2018-05-02 10:04:28 -0500

    newv
    Co-Authored-By: Saúl Zamora Pérez <saulzamo@users.noreply.github.com>

--- go/testFuncs.go
+++ go/testFuncs.go
@@ -1,22 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"libs/getAllCandidatesTwitterData"
 )
 
-type TwitterUser struct {
-	FavouritesCount int    `json:"favourites_count"`
-	FollowersCount  int    `json:"followers_count"`
-	FriendsCount    int    `json:"friends_count"`
-	ID              int64  `json:"id"`
-	ScreenName      string `json:"screen_name"`
-	NoTweets        int    `json:"tweet_count"`
-	ListCount       int    `json:"list_count"`
-	IDCandidato     int    `json:"id_candidato"`
-	Date            string `json:"fecha"`
-	IDTransaccion   int    `json:"id_transaccion"`
-}
-
 func main() {
 	test := getAllCandidatesTwitterData.GetAllCandidatesTwitterData()
+	fmt.Println(test)
 }
