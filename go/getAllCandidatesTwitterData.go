38df860a086fde17a726d1adafcfecfdf0557b5a
cbarron-siayec <38666243+cbarron-siayec@users.noreply.github.com>
2018-05-02 12:06:18 -0500

    Update getAllCandidatesTwitterData.go

--- go/getAllCandidatesTwitterData.go
+++ go/getAllCandidatesTwitterData.go
@@ -10,19 +10,19 @@ import (
 
 //Structure passed from database
 type TwitterUser struct {
-	FavouritesCount int    `json:"favourites_count"`
-	FollowersCount  int    `json:"followers_count"`
-	FriendsCount    int    `json:"friends_count"`
-	ID              int64  `json:"id"`
-	ScreenName      string `json:"screen_name"`
-	NoTweets        int    `json:"tweet_count"`
-	ListCount       int    `json:"list_count"`
-	IDCandidato     int    `json:"id_candidato"`
-	Date            string `json:"fecha"`
-	IDTransaccion   int    `json:"id_transaccion"`
+	FavouritesCount []int    `json:"favourites_count"`
+	FollowersCount  []int    `json:"followers_count"`
+	FriendsCount    []int    `json:"friends_count"`
+	ID              []int64  `json:"id"`
+	ScreenName      []string `json:"screen_name"`
+	NoTweets        []int    `json:"tweet_count"`
+	ListCount       []int    `json:"list_count"`
+	IDCandidato     []int    `json:"id_candidato"`
+	Date            []string `json:"fecha"`
+	IDTransaccion   []int    `json:"id_transaccion"`
 }
 
-func GetAllCandidatesTwitterData() []TwitterUser {
+func GetAllCandidatesTwitterData() TwitterUser {
 	// Open up our database connection.
 	connString := "root:D3m0S14y3c@(172.17.2.168:3306)/candidatos"
 	db, err := sql.Open("mysql", connString)
@@ -38,17 +38,14 @@ func GetAllCandidatesTwitterData() []TwitterUser {
 	}
 	// ORDEEN INSERT CANDIDATO: idCandidato int,friends int, favorites int, followers int, lists int, tweets int
 	// NOMBRES ESTRUCTURA USER: IDCandidato,FriendsCount,FavouritesCount,FollowersCount,ListCount,NoTweets
-	var resultsTwitterUser []TwitterUser
-	var int I
-	I = 0
-	for results.Next() {
+	var resultsTwitterUser TwitterUser
+	for i,results.Next():=0,_ {
 		// for each row, scan the result into our tag composite object
-		err = results.Scan(&resultsTwitterUser[I].IDTransaccion, &resultsTwitterUser[I].IDCandidato[I], &resultsTwitterUser[I].FriendsCount, &resultsTwitterUser[I].FavouritesCount, &resultsTwitterUser[I].FollowersCount, &resultsTwitterUser[I].ListCount, &resultsTwitterUser[I].NoTweets, &resultsTwitterUser[I].Date)
+		err = results.Scan(&resultsTwitterUser.IDTransaccion, &resultsTwitterUser.IDCandidato, &resultsTwitterUser.FriendsCount, &resultsTwitterUser.FavouritesCount, &resultsTwitterUser.FollowersCount, &resultsTwitterUser.ListCount, &resultsTwitterUser.NoTweets, &resultsTwitterUser.Date)
 		if err != nil {
 			panic(err.Error()) // proper error handling instead of panic in your app
 		}
 		// and then print out the tag's Name attribute
-		i++
 	}
 	return resultsTwitterUser
 }
