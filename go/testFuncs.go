4aeb02420b8a5fb075c1dacefde218889a92d015
cbarron-siayec <38666243+cbarron-siayec@users.noreply.github.com>
2018-05-02 12:54:10 -0500

    Update testFuncs.go

--- go/testFuncs.go
+++ go/testFuncs.go
@@ -7,7 +7,14 @@ import (
 
 func main() {
 	test := getAllCandidatesTwitterData.GetAllCandidatesTwitterData()
-
-	fmt.Println(test.FollowersCount)
-	fmt.Println(test.FriendsCount)
+	for i:= 0;i<len(test);i++{
+		fmt.Println(test.IDTransaccion[i])
+		fmt.Println(test.IDCandidato[i])
+		fmt.Println(test.FriendsCount[i])
+		fmt.Println(test.FavouritesCount[i])
+		fmt.Println(test.FollowersCount[i])
+		fmt.Println(test.ListCount[i])
+		fmt.Println(test.NoTweets[i])
+		fmt.Println(test.Date[i])
+	}
 }
