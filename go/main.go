b3375c60af82675f03b415b48dd11780193118d0
cbarron-siayec <38666243+cbarron-siayec@users.noreply.github.com>
2018-05-02 13:32:55 -0500

    Create main.go

---
+++ go/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, r.URL.Path[1:])
+	})
+
+  log.Fatal(http.ListenAndServe(":80", nil))
+
+}
