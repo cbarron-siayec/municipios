e7133ac2e76becb77559282c1da6166cd799f1ca
cbarron-siayec <38666243+cbarron-siayec@users.noreply.github.com>
2018-05-02 10:01:33 -0500

    Nuevas versiones
    Co-Authored-By: Saúl Zamora Pérez <saulzamo@users.noreply.github.com>

--- go/testFuncs.go
+++ go/testFuncs.go
@@ -1,32 +1,23 @@
 package main
 
 import (
-	"fmt"
-	"libs/twitterData"
-	"libs/insertCandidato"
+	"libs/getAllCandidatesTwitterData"
 )
 
+type TwitterUser struct {
+	FavouritesCount int    `json:"favourites_count"`
+	FollowersCount  int    `json:"followers_count"`
+	FriendsCount    int    `json:"friends_count"`
+	ID              int64  `json:"id"`
+	ScreenName      string `json:"screen_name"`
+	NoTweets        int    `json:"tweet_count"`
+	ListCount       int    `json:"list_count"`
+	IDCandidato     int    `json:"id_candidato"`
+	Date            string `json:"fecha"`
+	IDTransaccion   int    `json:"id_transaccion"`
+}
+
 func main() {
-	amlo := twitterData.GetData("D3hzZ2y3AlvG8o8Xw8SE2fxvZ","gpAkuWx7cyfGY6m6XffqcDyxNdwZRxhoCGDGS7ra54tCp1BHY5","2253585536-Yy1AxlyCNmEfEyJIOmhByqejD7XMXTpBIIHXoFW","Ha8IhwH4YElQwP2K3k3yKeT4EyDvZfd4EuxadGcddII9e","lopezobrador_", 1)
-	rac := twitterData.GetData("D3hzZ2y3AlvG8o8Xw8SE2fxvZ","gpAkuWx7cyfGY6m6XffqcDyxNdwZRxhoCGDGS7ra54tCp1BHY5","2253585536-Yy1AxlyCNmEfEyJIOmhByqejD7XMXTpBIIHXoFW","Ha8IhwH4YElQwP2K3k3yKeT4EyDvZfd4EuxadGcddII9e","RicardoAnayaC", 2)
-	jamk := twitterData.GetData("D3hzZ2y3AlvG8o8Xw8SE2fxvZ","gpAkuWx7cyfGY6m6XffqcDyxNdwZRxhoCGDGS7ra54tCp1BHY5","2253585536-Yy1AxlyCNmEfEyJIOmhByqejD7XMXTpBIIHXoFW","Ha8IhwH4YElQwP2K3k3yKeT4EyDvZfd4EuxadGcddII9e","JoseAMeadeK", 3)
-	mz := twitterData.GetData("D3hzZ2y3AlvG8o8Xw8SE2fxvZ","gpAkuWx7cyfGY6m6XffqcDyxNdwZRxhoCGDGS7ra54tCp1BHY5","2253585536-Yy1AxlyCNmEfEyJIOmhByqejD7XMXTpBIIHXoFW","Ha8IhwH4YElQwP2K3k3yKeT4EyDvZfd4EuxadGcddII9e","Mzavalagc", 4)
-	bronco := twitterData.GetData("D3hzZ2y3AlvG8o8Xw8SE2fxvZ","gpAkuWx7cyfGY6m6XffqcDyxNdwZRxhoCGDGS7ra54tCp1BHY5","2253585536-Yy1AxlyCNmEfEyJIOmhByqejD7XMXTpBIIHXoFW","Ha8IhwH4YElQwP2K3k3yKeT4EyDvZfd4EuxadGcddII9e","JaimeRdzNL", 5)
-	fmt.Println(amlo)
-	fmt.Println(rac)
-	fmt.Println(jamk)
-	fmt.Println(mz)
-	fmt.Println(bronco)
-	// ORDEEN INSERT CANDIDATO: idCandidato int,friends int, favorites int, followers int, lists int, tweets int
-	// NOMBRES ESTRUCTURA USER: IDCandidato,FriendsCount,FavouritesCount,FollowersCount,ListCount,NoTweets
-	//PARA AMLO
-	insertCandidato.InsertCandidato(amlo.IDCandidato,amlo.FriendsCount,amlo.FavouritesCount,amlo.FollowersCount,amlo.ListCount,amlo.NoTweets)
-	//PARA RAC
-	insertCandidato.InsertCandidato(rac.IDCandidato,rac.FriendsCount,rac.FavouritesCount,rac.FollowersCount,rac.ListCount,rac.NoTweets)
-	//PARA JAMK
-	insertCandidato.InsertCandidato(jamk.IDCandidato,jamk.FriendsCount,jamk.FavouritesCount,jamk.FollowersCount,jamk.ListCount,jamk.NoTweets)
-	//PARA MZ
-	insertCandidato.InsertCandidato(mz.IDCandidato,mz.FriendsCount,mz.FavouritesCount,mz.FollowersCount,mz.ListCount,mz.NoTweets)
-	//PARA BRONCO
-	insertCandidato.InsertCandidato(bronco.IDCandidato,bronco.FriendsCount,bronco.FavouritesCount,bronco.FollowersCount,bronco.ListCount,bronco.NoTweets)
+	var test TwitterUser
+	test = getAllCandidatesTwitterData.GetAllCandidatesTwitterData()
 }
